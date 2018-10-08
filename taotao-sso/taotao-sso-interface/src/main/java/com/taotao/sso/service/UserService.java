fe013e7d9bcf1ea5141d19d5b0e329dec1ec28d1
thinkal01 <875535828@qq.com>
2018-10-08 18:53:23 +0800

    youfen

--- taotao-sso/taotao-sso-interface/src/main/java/com/taotao/sso/service/UserService.java
+++ taotao-sso/taotao-sso-interface/src/main/java/com/taotao/sso/service/UserService.java
@@ -5,8 +5,11 @@ import com.taotao.pojo.TbUser;
 
 public interface UserService {
 
-	TaotaoResult checkData(String data, int type);
-	TaotaoResult register(TbUser user);
-	TaotaoResult login(String username, String password);
-	TaotaoResult getUserByToken(String token);
+    TaotaoResult checkData(String data, int type);
+
+    TaotaoResult register(TbUser user);
+
+    TaotaoResult login(String username, String password);
+
+    TaotaoResult getUserByToken(String token);
 }
